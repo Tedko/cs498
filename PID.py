84ce7babd44224f627b608f1f4565129bc270f2b
Han <yanhan02008@gmail.com>
2015-09-10 22:44:02 -0500

    chn ignr ; updt PID

--- PID.py
+++ PID.py
@@ -1,11 +1,12 @@
 import imp, sys
+import Ckpt as Ckpt
 
 class PID ():
 
 	def __init__(sf):
+		sf.altitude = fDat.altitude
+		print(sf.altitude)
 		sf.beta = 1.0
 		sf.gamma = 0.0
 		sf.alpha = 1.0
-		
-
-
+	def pidSpeed()
