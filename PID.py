a78260637f22de57e4359a4b5cb711631e6d9f12
Han <yanhan02008@gmail.com>
2015-09-14 20:09:01 -0500

    change pid

--- PID.py
+++ PID.py
@@ -3,10 +3,9 @@ import Ckpt as Ckpt
 
 class PID ():
 
-	def __init__(sf):
-		sf.altitude = fDat.altitude
-		print(sf.altitude)
-		sf.beta = 1.0
-		sf.gamma = 0.0
-		sf.alpha = 1.0
-	def pidSpeed()
+	def __init__(sf,coeff):
+		sf.cumError = 0
+		sf.coeff = coeff
+	def pid(error,time):
+		sf.cumError += error
+		return (cumError/time)*sf.coeff
