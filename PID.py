a593b46c8bc40a24cab52cb2ae1512ade5cba8ef
Han <yanhan02008@gmail.com>
2015-09-15 19:41:00 -0500

    try use new pid

--- PID.py
+++ PID.py
@@ -5,11 +5,21 @@ class PID ():
 
 	def __init__(sf,coeffP,coeffI,coeffD):
 		sf.cumError = 0
-		sf.coeff = coeff
-
+		sf.coeffP = coeffP
+		sf.coeffI = coeffI
+		sf.coeffD = coeffD
+		sf.lastError = 0
 	def pid(sf,error,time):
 		sf.cumError += error
-		return error*sf.coeffP + (sf.cumError/time)*sf.coeff
+		ret = error*sf.coeffP + (sf.cumError*time)*sf.coeffI + (sf.lastError-error)/time*sf.coeffD
 
+		print("P:",error*sf.coeffP)
+		print("I:",(sf.cumError*time)*sf.coeffI )
+		print("time:", time )
+		print("cumError:", (sf.cumError*time) )
+		print("D:",(sf.lastError-error)/time*sf.coeffD)
+
+		sf.lastError = error
+		return ret
 	def pidClear(sf):
 		sf.cumError = 0
